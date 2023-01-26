be7e8a8831db1cc215cd00e1a538f3784a5671f6
Victor Yuri <victoryuritc@yahoo.com>
2023-01-25 23:12:11 -0300

    feat: use $gt  $lt  $and $not $exists and fix lint

---
+++ challenges/desafio7.js
@@ -0,0 +1,16 @@
+db.produtos.find(
+  {
+    $and: [
+      {
+        vendidos: { $not: { $eq: 50 }},
+        tags: { $exists: false }
+      }
+    ]
+  },
+
+  {
+    nome: 1,
+    vendidos: 1,
+    _id: 0
+  }
+);
