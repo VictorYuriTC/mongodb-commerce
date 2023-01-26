b4f19363251fc1c7f08b585e9c8b292c927ba4c0
Victor Yuri <victoryuritc@yahoo.com>
2023-01-25 23:14:57 -0300

    fix: lint

--- challenges/desafio7.js
+++ challenges/desafio7.js
@@ -2,15 +2,15 @@ db.produtos.find(
   {
     $and: [
       {
-        vendidos: { $not: { $eq: 50 }},
-        tags: { $exists: false }
-      }
-    ]
+        vendidos: { $not: { $eq: 50 } },
+        tags: { $exists: false },
+      },
+    ],
   },
 
   {
     nome: 1,
     vendidos: 1,
-    _id: 0
-  }
+    _id: 0,
+  },
 );
