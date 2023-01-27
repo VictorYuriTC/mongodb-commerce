c9a6038809b0703ab356b9a10439bffac1058c02
Victor Yuri <victoryuritc@yahoo.com>
2023-01-27 14:48:54 -0300

    fix: remove find() after query

--- challenges/desafio3.js
+++ challenges/desafio3.js
@@ -1,16 +1,9 @@
-db.produtos
-  .find()
-  .sort(
-    {
-      vendidos: -1,
-    },
-  )
-  .limit(1)
-  .find(
-    {},
-    {
-      name: 1,
-      vendidos: 1,
-      _id: 0,
-    },
-  );
+db.produtos.find(
+  {
+
+  },
+
+  {
+    nome: 1, vendidos: 1, _id: 0,
+  },
+).sort({ vendidos: -1 }).limit(1);
