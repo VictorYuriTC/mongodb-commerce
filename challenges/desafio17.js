99fbc662902316e45f4557d3875734e6fd331749
Victor Yuri <victoryuritc@yahoo.com>
2023-01-28 20:50:08 -0300

    feat: use count() query inside insertOne()

---
+++ challenges/desafio17.js
@@ -0,0 +1,18 @@
+db.resumoProdutos.insertOne(
+  {
+    franquia: "McDonalds",
+    totalProdutos: db.produtos.count(),
+  },
+);
+
+db.resumoProdutos.find(
+  {
+
+  },
+
+  {
+    franquia: 1,
+    totalProdutos: 1,
+    _id: 0,
+  },
+);
